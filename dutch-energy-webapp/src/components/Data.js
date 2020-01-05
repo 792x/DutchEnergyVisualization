c824ba1e9b8656c0e0b413f546e80f6fae0252ce
caspertjuh <css@airmail.cc>
2020-01-05 15:56:38 +0100

    Update data panel selects

--- dutch-energy-webapp/src/components/Data.js
+++ dutch-energy-webapp/src/components/Data.js
@@ -16,7 +16,34 @@ const styles = theme => ({
     },
 });
 
+
+
+
 class Data extends Component {
+
+    state = {
+        loading: false,
+        energySourceSetting: 'electricity',
+        chartTypeSetting: 1,
+        timeframeSetting: '2019',
+    }
+
+    handleTimeFrameSettingChange = (e) => {
+        this.setState({ timeframeSetting: e.target.value });
+        console.log(e.target.value);
+    }
+
+    handleChartTypeSettingChange = (e) => {
+        this.setState({ chartTypeSetting: e.target.value });
+        console.log(e.target.value);
+    }
+
+    handleEnergySourceSettingChange = (e) => {
+        this.setState({ energySourceSetting: e.target.value });
+        console.log(e.target.value);
+    }
+
+
     render() {
         const { classes } = this.props;
         return (
@@ -24,47 +51,34 @@ class Data extends Component {
                 <Grid container direction="column" justify="flex-start" style={{ height: '100%' }}>
                     <Grid item style={{ height: '48px' }}>
                         <Grid container direction="row" justify="space-between" style={{ width: '100%', height: '48px' }} >
-                            <Grid item style={{marginTop:'10px', marginLeft: '10px'}}><Typography variant="h6">Data: <strong>Woensel</strong></Typography>
+                            <Grid item style={{ marginTop: '10px', marginLeft: '10px' }}><Typography variant="h6">Data: <strong>Woensel</strong></Typography>
                             </Grid>
                             <Grid item>
                                 <Grid container direction="row" justify="flex-end">
-                                    <Grid item  style={{paddingRight: '20px'}} >
-                                        <FormControl className={classes.formControl}>
+                                    <Grid item style={{ paddingRight: '20px', width: '150px' }} >
+                                        <FormControl className={classes.formControl} fullWidth>
                                             <InputLabel htmlFor="outlined-age-native-simple">
                                                 Energy Source
                                             </InputLabel>
                                             <Select
-                                                style={{width:'150px'}}
                                                 native
-                                                // value={state.age}
-                                                // onChange={handleChange('age')}
-                                                // labelWidth={labelWidth}
-                                                inputProps={{
-                                                    name: 'age',
-                                                    id: 'outlined-age-native-simple',
-                                                }}
+                                                value={this.state.energySourceSetting}
+                                                onChange={this.handleEnergySourceSettingChange}
                                             >
-                                                <option value="" />
-                                                <option value={1}>Electricity</option>
-                                                <option value={2}>Gas</option>
-
+                                                <option value={'electricity'}>Electricity</option>
+                                                <option value={'gas'}>Gas</option>
                                             </Select>
                                         </FormControl>
                                     </Grid>
-                                    <Grid item style={{paddingRight: '20px'}}>
+                                    <Grid item style={{ paddingRight: '20px' }}>
                                         <FormControl >
                                             <InputLabel htmlFor="outlined-age-native-simple">
                                                 Timeframe
                                     </InputLabel>
                                             <Select
                                                 native
-                                                // value={state.age}
-                                                // onChange={handleChange('age')}
-                                                // labelWidth={labelWidth}
-                                                inputProps={{
-                                                    name: 'age',
-                                                    id: 'outlined-age-native-simple',
-                                                }}
+                                                value={this.state.timeframeSetting}
+                                                onChange={this.handleTimeFrameSettingChange}
                                             >
                                                 <option value={1}>2019</option>
                                                 <option value={2}>2018</option>
@@ -90,29 +104,13 @@ class Data extends Component {
                                     </InputLabel>
                                             <Select
                                                 native
-                                                // value={state.age}
-                                                // onChange={handleChange('age')}
-                                                // labelWidth={labelWidth}
-                                                inputProps={{
-                                                    name: 'age',
-                                                    id: 'outlined-age-native-simple',
-                                                }}
+                                                value={this.state.chartTypeSetting}
+                                                onChange={this.handleChartTypeSettingChange}
                                             >
-                                                <option value="" />
-                                                <option value={1}>Bar charts</option>
-                                                <option value={2}>2018</option>
-                                                <option value={3}>2017</option>
-                                                <option value={4}>2016</option>
-                                                <option value={5}>2015</option>
-                                                <option value={6}>2014</option>
-                                                <option value={7}>2013</option>
-                                                <option value={8}>2012</option>
-                                                <option value={9}>2011</option>
-                                                <option value={10}>2010</option>
-                                                <option value={11}>Last two years</option>
-                                                <option value={12}>Last three years</option>
-                                                <option value={13}>Last five years</option>
-                                                <option value={14}>All data</option>
+                                                <option value={1}>Trends</option>
+                                                <option value={2}>Bar charts</option>
+                                                <option value={3}>Pie charts</option>
+                                                <option value={4}>Distributions</option>
                                             </Select>
                                         </FormControl>
                                     </Grid>
